a916971bd8153fd79fc6408b9f593366b0133f39
sxd <1447707563@qq.com>
2018-01-30 11:48:33 +0800

    首页完成

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,36 +1,43 @@
 <template>
   <section class="container">
-    <h1 class="title">
-      {{ error.statusCode }}
-    </h1>
-    <h2 class="info">
-      {{ error.message }}
-    </h2>
+    <div class="logo" style="height: 100px;text-align: left;padding-top: 50px;">
+      <nuxt-link to="/">
+        <img height="100%" src="../assets/img/error-logo.png"/>
+      </nuxt-link>
+    </div>
+    <div class="content" v-if="error.statusCode == 404">
+      <img class="content-img" width="30%" src="../assets/img/error.jpg" alt="" />
+      <h3>您要找的页面可能还在火星</h3>
+      <p>可能是因为您的链接地址有误、该文章已经被作者删除或转为私密状态。</p>
+    </div>
     <nuxt-link class="button" to="/" v-if="error.statusCode === 404">
-      Homepage
+      <input type="button" class="btn btn-success" value="回到❥简书首页" />
     </nuxt-link>
   </section>
 </template>
 <script>
-export default {
-  props: ['error']
-}
+    export default {
+        props: ['error']
+    }
 </script>
 
 <style scoped>
-.title
-{
-  margin-top: 15px;
-  font-size: 5em;
-}
-.info
-{
-  font-weight: 300;
-  color: #9aabb1;
-  margin: 0;
-}
-.button
-{
-  margin-top: 50px;
-}
+  .container{
+    text-align: center;
+  }
+  .content{
+    text-align: center;
+  }
+  .content h3,p{
+    padding-top: 20px;
+  }
+  .content p{
+    color: rgba(0,0,0,.35);
+    margin-bottom: 50px;
+  }
+  @media (max-width: 768px) {
+    .logo{
+      display: none;
+    }
+  }
 </style>
